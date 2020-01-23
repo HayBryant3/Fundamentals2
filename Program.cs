d677695836d87dc33790bd2841310218079568cb
HayBryant3 <59876349+HayBryant3@users.noreply.github.com>
2020-01-23 09:46:36 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Fundamentals2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DateTime is the type of the purchaseData variable.
+            DateTime purchaseDate = DateTime.Now;
+
+            /*
+                string is the type of the lastName variable. It
+                tells the compiler that the lastName variable can
+                ONLY hold a string value.
+            */
+            string lastName = "Bryant";
+
+            /*
+                C# now supports implicitly typing of a variable. The
+                type of the variable will be determined, by the
+                compiler, at compile time.
+            */
+            var firstName = "Hailee";
+            var favoriteColor = "pink";
+
+            /*
+                String interpolation in C# is similar to string interpolation in JavaScript,
+                but there is a different syntax.
+
+                An equivalent statement in JavaScript would be:
+                console.log(`${firstName} ${lastName} purchased on ${purchaseDate}`);
+            */
+            Console.WriteLine($"Hailee {lastName} purchased on {purchaseDate}");
+            Console.Write($"{firstName}'s favorite color is {favoriteColor}");
+        }
+    }
+}
